12fa15c5a7ecdd88742e229f724a18077b9ef35e
kyab <kyossi212@gmail.com>
2014-05-13 00:06:58 +0900

    Add Arduino Sketch

---
+++ blink/blink.ino
@@ -0,0 +1,18 @@
+int led = 13;
+
+void setup() {
+
+  Serial.begin(9600);
+  Serial.println("hello");
+  Serial.print(sum(99,1),DEC);
+  pinMode(led, OUTPUT);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(led, HIGH);
+  delay(1000);
+  digitalWrite(led, LOW);
+  delay(1000);
+}
